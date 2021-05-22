87a0186bcfdb5c8c535d3ece64aaf0812ff9c226
pinpinpin <pinpinpin@odds.team>
2021-05-22 14:24:09 +0700

    add galaryCell layout and binding

---
+++ GallaryAssignment/cell/GallaryCell.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class GallaryCell: UITableViewCell {
+
+    @IBOutlet weak var gallaryImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var likeIconImageView: UIImageView!
+    @IBOutlet weak var likeCountLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
